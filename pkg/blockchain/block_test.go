85a9268790205da59eb0e410f2420bf2c2ceac44
perkzen <perko.domen@gmail.com>
2023-06-04 22:25:55 +0200

    feat: block_test.go

---
+++ pkg/blockchain/block_test.go
@@ -0,0 +1,60 @@
+package blockchain
+
+import "testing"
+
+func TestBlock_Hash(t *testing.T) {
+	b := &Block{
+		Transactions: []*Tx{},
+		PrevHash:     [32]byte{},
+		Timestamp:    0,
+		Nonce:        0,
+	}
+
+	a := &Block{
+		Transactions: []*Tx{
+			CoinbaseTx("a"),
+			CoinbaseTx("b"),
+		},
+		PrevHash:  [32]byte{},
+		Timestamp: 0,
+		Nonce:     0,
+	}
+
+	c := &Block{
+		Transactions: []*Tx{
+			CoinbaseTx("a"),
+			CoinbaseTx("b"),
+		},
+		PrevHash:  [32]byte{},
+		Timestamp: 0,
+		Nonce:     0,
+	}
+
+	d := &Block{
+		Transactions: []*Tx{
+			CoinbaseTx("b"),
+			CoinbaseTx("a"),
+		},
+		PrevHash:  [32]byte{},
+		Timestamp: 0,
+		Nonce:     0,
+	}
+
+	hash := b.Hash()
+
+	if hash == [32]byte{} {
+		t.Error("Hash should not be empty")
+	}
+
+	if hash != b.Hash() {
+		t.Error("Hash should be the same")
+	}
+
+	if a.Hash() != c.Hash() {
+		t.Error("Hash should be the same")
+	}
+
+	if a.Hash() == d.Hash() {
+		t.Error("Hash should be different")
+	}
+}
