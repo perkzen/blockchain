2e67ee2d25ba4a0e178ee2d6458586fd8c8ba819
perkzen <perko.domen@gmail.com>
2023-06-04 22:36:26 +0200

    refactor: remove unused constant

--- pkg/blockchain/constants.go
+++ pkg/blockchain/constants.go
@@ -10,9 +10,6 @@ const (
 	// MINING_DIFFICULTY
 	// determines the number of zeros on the beginning of the hash
 	MINING_DIFFICULTY = 3
-	// MINING_SENDER
-	//  is the address from where you receive mining reward
-	MINING_SENDER = "THE BLOCKCHAIN"
 	// COINBASE_REWARD
 	// is the amount of coins received upon solving a block
 	COINBASE_REWARD = 0.1
