155eadd0837fdd989ede54163f69d446e9e737d4
perkzen <perko.domen@gmail.com>
2023-06-24 10:00:46 +0200

    feat: add mutex

--- pkg/server/nodes.go
+++ pkg/server/nodes.go
@@ -3,14 +3,26 @@ package server
 import (
 	"fmt"
 	"strconv"
+	"sync"
 )
 
+var mut sync.Mutex
+
 func (s *Server) addNode(addr string) {
+	mut.Lock()
+	defer mut.Unlock()
+
+	if addr == fmt.Sprintf("localhost:%d", s.Port()) {
+		return
+	}
+
 	client := newWebSocketClient(addr)
 	s.nodes[addr] = client
 }
 
 func (s *Server) removeNode(addr string) {
+	mut.Lock()
+	defer mut.Unlock()
 	delete(s.nodes, addr)
 }
 
@@ -24,7 +36,9 @@ func (s *Server) connectToKnownNodes() {
 	for addr := range s.nodes {
 		connAddr := fmt.Sprintf("localhost:%d", s.Port())
 		client := newWebSocketClient(addr)
+		mut.Lock()
 		s.nodes[addr] = client
+		mut.Unlock()
 		emitEvent(client, CONNECT, connAddr)
 	}
 }
